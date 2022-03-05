1b207c13e64143ed32432e2d95b14e7674dfe688
Gabriel PERINO <perinogabriel1@gmail.com>
2022-03-05 02:53:38 +0100

    [clients] : add basic form to select customers

---
+++ Clients.cs
@@ -0,0 +1,50 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutofactApp
+{
+    public partial class clients : Form
+    {
+        public clients()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string cs = "server=localhost;user=root;password=;database=autofact";
+            MySqlConnection connection = new MySqlConnection(cs);
+            connection.Open();
+            SelectClient.Rows.Clear();
+            MySqlCommand requete = new MySqlCommand("select * from client");
+            requete.Connection = connection;
+            MySqlDataReader reader = requete.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Object[] values = new object[reader.FieldCount];
+                reader.GetValues(values);
+                SelectClient.Rows.Add(values);
+            }
+        }
+
+        private void Logout_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void BackMenu_Click(object sender, EventArgs e)
+        {
+            Form Menu = new Menu();
+            Menu.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ clients.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace AutofactApp
+{
+    partial class clients
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SelectClient = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BackMenu = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.SelectClient)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SelectClient
+            // 
+            this.SelectClient.AllowUserToOrderColumns = true;
+            this.SelectClient.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.SelectClient.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.SelectClient.Location = new System.Drawing.Point(74, 149);
+            this.SelectClient.Name = "SelectClient";
+            this.SelectClient.RowTemplate.Height = 25;
+            this.SelectClient.Size = new System.Drawing.Size(543, 150);
+            this.SelectClient.TabIndex = 1;
+            // 
+            // id
+            // 
+            this.id.HeaderText = "id";
+            this.id.Name = "id";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "nom";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "prenom";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "telephone";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "mail";
+            this.Column5.Name = "Column5";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(271, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 28);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Liste des clients";
+            // 
+            // BackMenu
+            // 
+            this.BackMenu.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BackMenu.Location = new System.Drawing.Point(30, 22);
+            this.BackMenu.Name = "BackMenu";
+            this.BackMenu.Size = new System.Drawing.Size(149, 33);
+            this.BackMenu.TabIndex = 4;
+            this.BackMenu.Text = "Retour au menu principal";
+            this.BackMenu.UseVisualStyleBackColor = true;
+            this.BackMenu.Click += new System.EventHandler(this.BackMenu_Click);
+            // 
+            // clients
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(705, 391);
+            this.Controls.Add(this.BackMenu);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SelectClient);
+            this.Name = "clients";
+            this.Text = "Clients";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.SelectClient)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView SelectClient;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BackMenu;
+    }
+}
+
