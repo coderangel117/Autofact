293a055a2967ff3033d31d8a98c8d3d208f7f245
Gabriel PERINO <gabriel.perino@lyceefulbert.fr>
2022-04-28 09:02:45 +0200

    [commandesparclients] : create the form that list the command per customer

---
+++ commandesparclient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutofactApp
+{
+    public partial class commandesparclient : Form
+    {
+        public commandesparclient()
+        {
+            InitializeComponent();
+        }
+
+        private void BackMenu_Click(object sender, EventArgs e)
+        {
+            new Menu().Show();
+            this.Hide();
+        }
+    }
+}
