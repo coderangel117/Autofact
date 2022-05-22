92e1dd75141d078e90091295a077690891467705
Gabriel PERINO <gabriel.perino@lyceefulbert.fr>
2022-05-22 23:24:49 +0200

    [modifierprestation] : create file /  add input / add update query  / add second constructor to receive data list from prestations form

---
+++ ModifierClient.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace AutofactApp
+{
+    partial class ModifierClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.idCustomer = new System.Windows.Forms.TextBox();
+            this.NewMail = new System.Windows.Forms.Label();
+            this.NewTel = new System.Windows.Forms.Label();
+            this.NewMailText = new System.Windows.Forms.TextBox();
+            this.NewTelText = new System.Windows.Forms.TextBox();
+            this.NewPrenom = new System.Windows.Forms.Label();
+            this.NewNom = new System.Windows.Forms.Label();
+            this.NewPrenomText = new System.Windows.Forms.TextBox();
+            this.NewNomText = new System.Windows.Forms.TextBox();
+            this.AddConfirm = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // idCustomer
+            // 
+            this.idCustomer.Enabled = false;
+            this.idCustomer.Location = new System.Drawing.Point(205, 22);
+            this.idCustomer.Name = "idCustomer";
+            this.idCustomer.Size = new System.Drawing.Size(58, 23);
+            this.idCustomer.TabIndex = 29;
+            this.idCustomer.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.idCustomer.Visible = false;
+            // 
+            // NewMail
+            // 
+            this.NewMail.AutoSize = true;
+            this.NewMail.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.NewMail.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.NewMail.Location = new System.Drawing.Point(26, 290);
+            this.NewMail.Name = "NewMail";
+            this.NewMail.Size = new System.Drawing.Size(99, 21);
+            this.NewMail.TabIndex = 28;
+            this.NewMail.Text = "Adresse mail";
+            // 
+            // NewTel
+            // 
+            this.NewTel.AutoSize = true;
+            this.NewTel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.NewTel.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.NewTel.Location = new System.Drawing.Point(45, 223);
+            this.NewTel.Name = "NewTel";
+            this.NewTel.Size = new System.Drawing.Size(80, 21);
+            this.NewTel.TabIndex = 27;
+            this.NewTel.Text = "Téléphone";
+            // 
+            // NewMailText
+            // 
+            this.NewMailText.Location = new System.Drawing.Point(158, 292);
+            this.NewMailText.Name = "NewMailText";
+            this.NewMailText.Size = new System.Drawing.Size(167, 23);
+            this.NewMailText.TabIndex = 24;
+            this.NewMailText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // NewTelText
+            // 
+            this.NewTelText.Location = new System.Drawing.Point(158, 219);
+            this.NewTelText.Name = "NewTelText";
+            this.NewTelText.Size = new System.Drawing.Size(167, 23);
+            this.NewTelText.TabIndex = 23;
+            this.NewTelText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // NewPrenom
+            // 
+            this.NewPrenom.AutoSize = true;
+            this.NewPrenom.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.NewPrenom.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.NewPrenom.Location = new System.Drawing.Point(60, 81);
+            this.NewPrenom.Name = "NewPrenom";
+            this.NewPrenom.Size = new System.Drawing.Size(65, 21);
+            this.NewPrenom.TabIndex = 26;
+            this.NewPrenom.Text = "Prenom";
+            // 
+            // NewNom
+            // 
+            this.NewNom.AutoSize = true;
+            this.NewNom.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.NewNom.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.NewNom.Location = new System.Drawing.Point(80, 157);
+            this.NewNom.Name = "NewNom";
+            this.NewNom.Size = new System.Drawing.Size(45, 21);
+            this.NewNom.TabIndex = 25;
+            this.NewNom.Text = "Nom";
+            // 
+            // NewPrenomText
+            // 
+            this.NewPrenomText.Location = new System.Drawing.Point(158, 83);
+            this.NewPrenomText.Name = "NewPrenomText";
+            this.NewPrenomText.Size = new System.Drawing.Size(167, 23);
+            this.NewPrenomText.TabIndex = 22;
+            this.NewPrenomText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // NewNomText
+            // 
+            this.NewNomText.Location = new System.Drawing.Point(158, 157);
+            this.NewNomText.Name = "NewNomText";
+            this.NewNomText.Size = new System.Drawing.Size(167, 23);
+            this.NewNomText.TabIndex = 21;
+            this.NewNomText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // AddConfirm
+            // 
+            this.AddConfirm.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.AddConfirm.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.AddConfirm.Location = new System.Drawing.Point(251, 360);
+            this.AddConfirm.Name = "AddConfirm";
+            this.AddConfirm.Size = new System.Drawing.Size(129, 33);
+            this.AddConfirm.TabIndex = 32;
+            this.AddConfirm.Text = "Confirmer";
+            this.AddConfirm.UseVisualStyleBackColor = true;
+            this.AddConfirm.Click += new System.EventHandler(this.AddConfirm_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.button2.Location = new System.Drawing.Point(45, 360);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 33);
+            this.button2.TabIndex = 34;
+            this.button2.Text = "Annuler";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // ModifierClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 479);
+            this.Controls.Add(this.AddConfirm);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.idCustomer);
+            this.Controls.Add(this.NewMail);
+            this.Controls.Add(this.NewTel);
+            this.Controls.Add(this.NewMailText);
+            this.Controls.Add(this.NewTelText);
+            this.Controls.Add(this.NewPrenom);
+            this.Controls.Add(this.NewNom);
+            this.Controls.Add(this.NewPrenomText);
+            this.Controls.Add(this.NewNomText);
+            this.Name = "ModifierClient";
+            this.Text = "ModifierClient";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox idCustomer;
+        private System.Windows.Forms.Label NewMail;
+        private System.Windows.Forms.Label NewTel;
+        private System.Windows.Forms.TextBox NewMailText;
+        private System.Windows.Forms.TextBox NewTelText;
+        private System.Windows.Forms.Label NewPrenom;
+        private System.Windows.Forms.Label NewNom;
+        private System.Windows.Forms.TextBox NewPrenomText;
+        private System.Windows.Forms.TextBox NewNomText;
+        private System.Windows.Forms.Button AddConfirm;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ ModifierClient.cs
@@ -0,0 +1,69 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutofactApp
+{
+    public partial class ModifierClient : Form
+    {
+        public ModifierClient(List<string> parametres) : this()
+        {
+            idCustomer.Text= parametres[0];
+            NewNomText.Text = parametres[1];
+            NewPrenomText.Text = parametres[2];
+            NewTelText.Text = parametres[3];
+            NewMailText.Text = parametres[4];
+            
+        }
+        public ModifierClient()
+        {
+            InitializeComponent();
+        }
+
+        private void BackMenu_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            new Menu().Show();
+        }
+        private void AddConfirm_Click(object sender, EventArgs e)
+        {
+
+            if (idCustomer.Text != "" && NewNomText.Text != "" && NewPrenomText.Text != "" && NewTelText.Text != "" && NewMailText.Text != "")
+            {
+                string cs = "server=localhost;user=root;password=;database=autofact";
+                MySqlConnection connection = new MySqlConnection(cs);
+                connection.Open();
+                MySqlCommand cmd = new MySqlCommand("update client set nom=@nom,prenom=@prenom,telephone=@telephone, mail=@mail where idC=@id", connection);
+                cmd.Parameters.AddWithValue("@id", idCustomer.Text);
+                cmd.Parameters.AddWithValue("@nom", NewNomText.Text);
+                cmd.Parameters.AddWithValue("@prenom", NewPrenomText.Text);
+                cmd.Parameters.AddWithValue("@telephone", NewTelText.Text);
+                cmd.Parameters.AddWithValue("@mail", NewMailText.Text);
+                cmd.ExecuteNonQuery();
+                connection.Close();
+                this.Hide();
+                new clients().Show();
+                MessageBox.Show("Les changements ont bien été enregistrées");
+            }
+            else
+            {
+                MessageBox.Show("Veuillez saisir les informations !");
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            new clients().Show();
+            this.Hide();
+        }
+    }
+}
