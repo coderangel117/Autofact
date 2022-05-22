dcfb41e0d1d1c14d21de9f39a03e0c579fdce722
Gabriel PERINO <gabriel.perino@lyceefulbert.fr>
2022-05-22 23:25:55 +0200

    [modifierclient] : create file /  add input / add update query  / add second constructor to receive list data from clients form

---
+++ ModifierPrestation.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace AutofactApp
+{
+    partial class ModifierPrestation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddConfirm = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.idCustomer = new System.Windows.Forms.TextBox();
+            this.NewMail = new System.Windows.Forms.Label();
+            this.NewTel = new System.Windows.Forms.Label();
+            this.PriceText = new System.Windows.Forms.TextBox();
+            this.NewNom = new System.Windows.Forms.Label();
+            this.LabelText = new System.Windows.Forms.TextBox();
+            this.DetailsText = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // AddConfirm
+            // 
+            this.AddConfirm.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.AddConfirm.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.AddConfirm.Location = new System.Drawing.Point(249, 349);
+            this.AddConfirm.Name = "AddConfirm";
+            this.AddConfirm.Size = new System.Drawing.Size(129, 33);
+            this.AddConfirm.TabIndex = 5;
+            this.AddConfirm.Text = "Confirmer";
+            this.AddConfirm.UseVisualStyleBackColor = true;
+            this.AddConfirm.Click += new System.EventHandler(this.AddConfirm_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.button2.Location = new System.Drawing.Point(66, 349);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 33);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Annuler";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // idCustomer
+            // 
+            this.idCustomer.Enabled = false;
+            this.idCustomer.Location = new System.Drawing.Point(249, 12);
+            this.idCustomer.Name = "idCustomer";
+            this.idCustomer.Size = new System.Drawing.Size(77, 23);
+            this.idCustomer.TabIndex = 43;
+            this.idCustomer.TabStop = false;
+            this.idCustomer.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.idCustomer.Visible = false;
+            // 
+            // NewMail
+            // 
+            this.NewMail.AutoSize = true;
+            this.NewMail.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.NewMail.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.NewMail.Location = new System.Drawing.Point(91, 293);
+            this.NewMail.Name = "NewMail";
+            this.NewMail.Size = new System.Drawing.Size(36, 21);
+            this.NewMail.TabIndex = 42;
+            this.NewMail.Text = "Prix";
+            // 
+            // NewTel
+            // 
+            this.NewTel.AutoSize = true;
+            this.NewTel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.NewTel.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.NewTel.Location = new System.Drawing.Point(78, 187);
+            this.NewTel.Name = "NewTel";
+            this.NewTel.Size = new System.Drawing.Size(57, 21);
+            this.NewTel.TabIndex = 41;
+            this.NewTel.Text = "Details";
+            // 
+            // PriceText
+            // 
+            this.PriceText.Location = new System.Drawing.Point(156, 291);
+            this.PriceText.Name = "PriceText";
+            this.PriceText.Size = new System.Drawing.Size(170, 23);
+            this.PriceText.TabIndex = 4;
+            this.PriceText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // NewNom
+            // 
+            this.NewNom.AutoSize = true;
+            this.NewNom.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.NewNom.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.NewNom.Location = new System.Drawing.Point(78, 101);
+            this.NewNom.Name = "NewNom";
+            this.NewNom.Size = new System.Drawing.Size(55, 21);
+            this.NewNom.TabIndex = 39;
+            this.NewNom.Text = "Libelle";
+            // 
+            // LabelText
+            // 
+            this.LabelText.Location = new System.Drawing.Point(156, 101);
+            this.LabelText.Name = "LabelText";
+            this.LabelText.Size = new System.Drawing.Size(170, 23);
+            this.LabelText.TabIndex = 2;
+            this.LabelText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // DetailsText
+            // 
+            this.DetailsText.Location = new System.Drawing.Point(156, 169);
+            this.DetailsText.Name = "DetailsText";
+            this.DetailsText.Size = new System.Drawing.Size(170, 75);
+            this.DetailsText.TabIndex = 44;
+            this.DetailsText.TabStop = false;
+            this.DetailsText.Text = "";
+            // 
+            // ModifierPrestation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 479);
+            this.Controls.Add(this.DetailsText);
+            this.Controls.Add(this.AddConfirm);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.idCustomer);
+            this.Controls.Add(this.NewMail);
+            this.Controls.Add(this.NewTel);
+            this.Controls.Add(this.PriceText);
+            this.Controls.Add(this.NewNom);
+            this.Controls.Add(this.LabelText);
+            this.Name = "ModifierPrestation";
+            this.Text = "ModifierPrestation";
+            this.Load += new System.EventHandler(this.ModifierPrestation_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddConfirm;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox idCustomer;
+        private System.Windows.Forms.Label NewMail;
+        private System.Windows.Forms.Label NewTel;
+        private System.Windows.Forms.TextBox PriceText;
+        private System.Windows.Forms.Label NewNom;
+        private System.Windows.Forms.TextBox LabelText;
+        private System.Windows.Forms.RichTextBox DetailsText;
+    }
+}
---
+++ ModifierPrestation.cs
@@ -0,0 +1,66 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace AutofactApp
+{
+    public partial class ModifierPrestation : Form
+    {
+        public ModifierPrestation(List<string> parametres) : this()
+        {
+            idCustomer.Text = parametres[0];
+            LabelText.Text = parametres[1];
+            DetailsText.Text = parametres[2];
+            PriceText.Text = parametres[3];
+
+        }
+        public ModifierPrestation()
+        {
+            InitializeComponent();
+        }
+
+        private void ModifierPrestation_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddConfirm_Click(object sender, EventArgs e)
+        {
+            if (idCustomer.Text != "" && LabelText.Text != "" && DetailsText.Text != "" && PriceText.Text != "")
+            {
+                string cs = "server=localhost;user=root;password=;database=autofact";
+                MySqlConnection connection = new MySqlConnection(cs);
+                connection.Open();
+                MySqlCommand cmd = new MySqlCommand("update prestation set LibellePrestation=@label,Description=@details,PrixHt=@price where Idp=@id", connection);
+                cmd.Parameters.AddWithValue("@id", idCustomer.Text);
+                cmd.Parameters.AddWithValue("@label", LabelText.Text);
+                cmd.Parameters.AddWithValue("@details", DetailsText.Text);
+                cmd.Parameters.AddWithValue("@price", PriceText.Text);
+                cmd.ExecuteNonQuery();
+                connection.Close();
+                new Prestations().Show();
+                this.Hide();
+                MessageBox.Show("Les changements ont bien été enregistrées");
+            }
+            else
+            {
+                MessageBox.Show("Veuillez saisir les informations !");
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            new Prestations().Show();
+            this.Hide();
+        }
+    }
+}
